ca79f84447510f19a7b113cf0c9fd9f64d35baec
Sigh <yxh644166727@gmail.com>
2021-04-03 21:32:14 -0400

    append comments

--- proxy/server.js
+++ proxy/server.js
@@ -4,13 +4,13 @@ var express = require('express')
 var app = express()
 
 var path = require('path')
-
+//Serving as static file
 var staticFilesPath = path.resolve(__dirname, '..', 'build')
-
+//Enable CORS
 app.use(cors())
 
 app.use(express.static(staticFilesPath))
-
+//Serving at /api from source https://www.tabtu.top/
 app.use('/api', proxy('https://www.tabtu.top/'))
-
+//Proxy listening on port 3001
 app.listen(3001, () => console.log('Server Started'))
