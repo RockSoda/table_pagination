c09791dd38542b199fd336f291a6d2a34377d769
Sigh <yxh644166727@gmail.com>
2021-04-03 21:45:25 -0400

    append comments

--- src/App.js
+++ src/App.js
@@ -7,30 +7,35 @@ import { uuid } from 'uuidv4';
 const API = 'api/pic/100.json'
 
 const App = () => {
-
+  //Create states
   const [posts, setPosts] = useState([])
   const [loading, setLoading] = useState(false)
   const [currentPage, setCurrentPage] = useState(1)
   const [postsPerPage, setPostsPerPage] = useState(20)
-
+  
+  //Fetching but only for once, working as componentDidMount
   useEffect(() => {
-
+    //Create an async fetch function (since [async, await] can only be used on a function)
     const fetchPosts = async () => {
       setLoading(true)
       const res = await axios.get(API)
       setPosts(res.data)
       setLoading(false)
     }
-
+    //Fetch
     fetchPosts()
   }, [])
-
+  
+  //Calculate the index of the last post on the page
   const indexOfLastPost = currentPage * postsPerPage
+  //Calculate the index of the first post on the page
   const indexOfFirstPost = indexOfLastPost - postsPerPage
+  //Slice the array which contains all the posts, from the index of the first post to index of the last post
+  //Which are the posts to render
   const currentPosts = posts.slice(indexOfFirstPost, indexOfLastPost)
-
+  //A function working as a prop, which will be triggered when one of the page number is clicked
   const paginate = (pageNumber) => setCurrentPage(pageNumber)
-
+  //List of table headings will be rendered
   const table_heads = ['Name', '_id', 'Index', 'Status', 'Picture', 'Tags']
 
   return (
